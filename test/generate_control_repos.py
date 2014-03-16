c27ee8b9718fb69cf48e5652e396d45ed34ce91f
Joshua Ryan Smith <joshua.r.smith@gmail.com>
2014-03-16 18:36:05 -0400

    Stub for method creating repo with three remotes.

---
+++ test/generate_control_repos.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Herein I generate control git repos to perform tests. I want to do this programatically so its auditable.
+"""
+
+import git
+
+def generate_three_remotes():
+    """
+    Generates a git repo with three remotes.
+    """
+    remotes = {"gethub": "git@gethub.com:username/three_remotes.git",
+               "example": "git@example.com:username/three_remotes.git",
+               "notaurl": "git@notaurl.com:username/three_remotes.git",}
+
+    repo_dirname = "three_remotes"
+    repo_tarfilename = repo_dirname + ".tar"
+
+    repo = git.Repo.init(repo_dirname)
+
+    # Archive repo.
+    repo.archive(open(repo_tarfilename, "w"))
