99e5d444f33875c3c9c8f9cc16cd6de3b7ed44d3
Joshua Ryan Smith <joshua.r.smith@gmail.com>
2014-03-17 00:31:23 -0400

    Method to generate repo with committed files, uncomitted files, and ignored files.

--- test/generate_control_repos.py
+++ test/generate_control_repos.py
@@ -5,6 +5,7 @@ Herein I generate control git repos to perform tests. I want to do this programa
 
 import git
 import tarfile
+import os
 
 def generate_repo_with_three_remotes():
     """
@@ -23,6 +24,56 @@ def generate_repo_with_three_remotes():
     # Add remotes
     for name, url in remotes.iteritems():
         repo.create_remote(name, url)
+
+    # Archive repo.
+    with tarfile.open(repo_targzfilename, "w:gz") as tar:
+        tar.add(repo_dirname)
+
+def generate_1_ignore_2_dirs_1_uncommit():
+    """
+    Repository with one ignored and one uncomitted file, two dirs.
+
+    The two directories will have files which are comitted to the repo. The `.gitignore` will be the uncomitted file, and a file named `ignored` will be ignored in gitignore.
+    """
+    repo_dirname = "1_ignore_2_dirs_1_uncommit"
+    repo_targzfilename = repo_dirname + ".tar.gz"
+
+    # Initialize repo
+    repo = git.Repo.init(repo_dirname)
+
+    # Files that will live in the repo.
+    gitignore = "ignored"
+    ignored_filename = "ignored"
+    dir_slug = "dir%s"
+
+    # Make .gitignore
+    filepath = os.path.join(repo_dirname, ".gitignore") 
+    with open(filepath, "w") as f:
+        f.write(gitignore)
+
+    # `touch` file `ignored`
+    filepath = os.path.join(repo_dirname, gitignore)
+    with open(filepath,"w") as f:
+        pass
+
+    # Create the two directories with files.
+    for indx in range(1,3):
+        dirname = dir_slug % indx
+        dirpath = os.path.join(repo_dirname, dirname)
+
+        os.mkdir(dirpath)
+
+        # `touch` file
+        file_path = os.path.join(dirpath, "file")
+        with open(file_path, "w") as f:
+            pass
+
+        # git add file
+        add_path = os.path.join(dirname, "file")
+        repo.git.add(add_path)
+
+    # Commit changes.
+    repo.git.commit(m = "Commit to repo.")
 
     # Archive repo.
     with tarfile.open(repo_targzfilename, "w:gz") as tar:
