6df86816fa52db024098b425efe6f604e5d2b664
Joshua Ryan Smith <joshua.r.smith@gmail.com>
2014-08-09 11:16:37 -0400

    Reordered methods. Purely cosmetic change.

--- gittool/gittool.py
+++ gittool/gittool.py
@@ -22,22 +22,6 @@ def list_tl_subdirs(repo):
     return dirnames
 
 
-def branchable_subdirs(repo):
-    """
-    List subdir names of repo which can be branches.
-    """
-    dirnames = list_tl_subdirs(repo)
-
-    # I know I need to throw out the `.git` from that list.
-    dirnames.remove(".git")
-
-    # I need to throw out directories no files at any nesting level. In other words, a directory can have an arbitrary number of nested directories, but no files.
-
-    # I also know that there's a possibility the repo has subdirectories that can't be split using `git subtree split`. For example these dirs may be in .gitignore (there are other cases).
-
-    return dirnames
-
-
 def empty_subdirs(repo):
     """
     List of top-level subdirectories which contain no files at any nesting level.
@@ -57,6 +41,22 @@ def empty_subdirs(repo):
             empty_paths.append(path)
 
     return empty_paths
+
+
+def branchable_subdirs(repo):
+    """
+    List subdir names of repo which can be branches.
+    """
+    dirnames = list_tl_subdirs(repo)
+
+    # I know I need to throw out the `.git` from that list.
+    dirnames.remove(".git")
+
+    # I need to throw out directories no files at any nesting level. In other words, a directory can have an arbitrary number of nested directories, but no files.
+
+    # I also know that there's a possibility the repo has subdirectories that can't be split using `git subtree split`. For example these dirs may be in .gitignore (there are other cases).
+
+    return dirnames
 
 
 def branches_from_subdirs(repo):
