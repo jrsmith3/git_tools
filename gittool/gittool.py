b3c05ec9c842e2128e76237ea5f803321e80894e
Joshua Ryan Smith <joshua.r.smith@gmail.com>
2014-08-08 21:19:22 -0400

    Method to return relative subdir names of a repo that can be branched with git-subtree.

--- gittool/gittool.py
+++ gittool/gittool.py
@@ -7,3 +7,18 @@ import os
 import logging
 import argparse
 import git
+
+def branchable_subdirs(repo):
+    """
+    List subdir names of repo which can be branches.
+    """
+    src_fqpn = repo.wd
+
+    dirnames = [f for f in os.listdir(src_fqpn) if os.path.isdir(os.path.join(src_fqpn,f))]
+
+    # I know I need to throw out the `.git` from that list.
+    dirnames.remove(".git")
+
+    # I also know that there's a possibility the repo has subdirectories that can't be split using `git subtree split`. For example these dirs may be in .gitignore (there are other cases).
+
+    return dirnames
